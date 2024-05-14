b15ebd82d4363ee02362fd6752ff4d765f4d7e88
Taco <115353463+iamxiaobing47@users.noreply.github.com>
2024-05-14 19:11:12 +0000

    add pojo

---
+++ date_time_zone/src/main/java/com/example/date_time_zone/DateTimeZoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.date_time_zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateTimeZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateTimeZoneApplication.class, args);
+	}
+
+}
---
+++ date_time_zone/src/test/java/com/example/date_time_zone/DateTimeZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.date_time_zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateTimeZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
